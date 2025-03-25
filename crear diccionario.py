34b074b22009c3f0ecf92f5446c79ed8d92b9879
Luisa-Avendano20 <ll.avendanof@uea.edu.ec>
2025-03-25 16:25:05 -0500

    Update crear diccionario.py

--- crear diccionario.py
+++ crear diccionario.py
@@ -8,7 +8,7 @@ informacion_personal = {
 
 # Acceder y Modificar Valores
 # Acceder al valor de la clave "ciudad" y modificarlo
-informacion_personal["ciudad"] = "Rusia"  # Cambiar la ciudad a Barcelona
+informacion_personal["ciudad"] = "Rusia"  # Cambiar la ciudad a Rusia
 
 # Agregar una nueva clave-valor para la "profesion"
 informacion_personal["profesion"] = "Desarrollador de Software"  # Actualizar la profesión
@@ -16,7 +16,7 @@ informacion_personal["profesion"] = "Desarrollador de Software"  # Actualizar la
 # Verificar Existencia de Claves
 # Verificar si la clave "numero de celular" existe en el diccionario
 if "numero de celular" not in informacion_personal:
-    informacion_personal["numero de celular"] = "0980185424"  # Agregar un número de teléfono ficticio
+    informacion_personal["numero de celular"] = "0980185424"  # Agregar un número de celular ficticio
 
 # Eliminar una Clave
 # Eliminar la clave "edad" del diccionario
